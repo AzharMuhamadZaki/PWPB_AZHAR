c05a332fc056e42c60b32a708951998f00eaa9f3
AzharMuhamadZaki <93692818+AzharMuhamadZaki@users.noreply.github.com>
2022-04-05 12:26:41 +0700

    Add files via upload

---
+++ latihan_ukkazhar/admin/config/Config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('baseurl','http://localhost/latihan_ukkazhar/public');
+
+//DB
+define('DB_HOST','localhost');
+define('DB_USER','root');
+define('DB_PASS','');
+define('DB_NAME','latihan_ukkazhar2');
